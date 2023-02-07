fad2668631f27d94f0586ea81099a04e9afa3e28
sivapriya0312 <113236672+sivapriya0312@users.noreply.github.com>
2023-02-07 22:37:20 +0530

    Add files via upload

---
+++ matrix.java
@@ -0,0 +1,46 @@
+
+
+
+              class Matrix{
+
+    public static void main(String args[]){
+
+          for(int i=1;i<=2;i++){
+
+            for(int j=0;j<3;j++){
+
+            if((i==1 && j==0)){
+
+            System.out.print("@");
+
+            }
+
+            else if(i==1 && j==2){
+
+            System.out.print("@");
+
+            }
+
+            else if(i==2 && j==1){
+
+            System.out.print("1");
+
+            }
+
+            else{
+
+            System.out.print(i);
+
+            }
+
+            }
+
+        System.out.println();
+
+}
+
+      }
+
+}
+
+
