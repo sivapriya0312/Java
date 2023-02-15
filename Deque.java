a0723e972284ed6998830d2223d72e809c0d4641
sivapriya0312 <113236672+sivapriya0312@users.noreply.github.com>
2023-02-15 15:13:30 +0530

    Add files via upload

---
+++ Deque.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Deque {
+    public static void main(String args[]){
+                ArrayDeque<String> dq = new ArrayDeque<String>();
+                dq.add("am");
+                dq.addFirst("I");
+                dq.addLast("Full Stack");
+                dq.add("Intern");
+
+                for (Iterator itr = dq.iterator();
+                     itr.hasNext();) {
+                    System.out.print(itr.next() + " ");
+                }
+
+                System.out.println();
+
+                for (Iterator itr = dq.descendingIterator();
+                     itr.hasNext();) {
+                    System.out.print(itr.next() + " ");
+                }
+            }
+        }
+
